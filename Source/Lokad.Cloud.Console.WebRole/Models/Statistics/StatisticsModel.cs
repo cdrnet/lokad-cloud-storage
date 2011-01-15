55ca9ad0d166d4fba3be64c4f3d15638fbd56fad
Christoph Ruegg <christoph.ruegg@lokad.com>
2011-01-15 08:35:43 +0000

    Cloud Console: Dropping placeholder Jobs and Statistics tabs (to be re-added later when implemented)

--- Source/Lokad.Cloud.Console.WebRole/Models/Statistics/StatisticsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lokad.Cloud.Management.Api10;
-
-namespace Lokad.Cloud.Console.WebRole.Models.Statistics
-{
-    public class StatisticsModel
-    {
-        public CloudServiceInfo[] Services;
-    }
-}
