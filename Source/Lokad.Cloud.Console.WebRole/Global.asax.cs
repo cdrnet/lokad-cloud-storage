516738b4772bda74734861e8e92f4db77c7e9699
Christoph Ruegg <christoph.ruegg@lokad.com>
2011-05-02 17:57:57 +0200

    Framework: Service Inspection now using Cecil instead of Reflection   (no more appdomain, no longer required to load dependent assemblies like autofac) Libraries: Changed to NuGet-managed package references, upgraded where available.

--- Source/Lokad.Cloud.Console.WebRole/Global.asax.cs
+++ Source/Lokad.Cloud.Console.WebRole/Global.asax.cs
@@ -1,14 +1,12 @@
-﻿using System.Reflection;
-using System.Threading;
+﻿using System.Threading;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Lokad.Cloud.Console.WebRole.Framework.Discovery;
-using Ninject;
-using Ninject.Web.Mvc;
 
 namespace Lokad.Cloud.Console.WebRole
 {
-    public class MvcApplication : NinjectHttpApplication
+    public class MvcApplication : HttpApplication
     {
         private CancellationTokenSource _discoveryCancellation;
 
@@ -33,17 +31,8 @@ namespace Lokad.Cloud.Console.WebRole
             routes.MapRoute("MenuByHostedService", "{controller}/{hostedServiceName}/{action}", new { controller = "Overview", action = "ByHostedService" });
         }
 
-        protected override IKernel CreateKernel()
+        protected void Application_Start()
         {
-            var kernel = new StandardKernel();
-            kernel.Load(Assembly.GetExecutingAssembly());
-            return kernel;
-        }
-
-        protected override void OnApplicationStarted()
-        {
-            base.OnApplicationStarted();
-
             AreaRegistration.RegisterAllAreas();
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
@@ -53,15 +42,13 @@ namespace Lokad.Cloud.Console.WebRole
             discovery.StartAutomaticCacheUpdate(_discoveryCancellation.Token);
         }
 
-        protected override void OnApplicationStopped()
+        protected void Application_End()
         {
             if (_discoveryCancellation != null)
             {
                 _discoveryCancellation.Cancel();
                 _discoveryCancellation = null;
             }
-
-            base.OnApplicationStopped();
         }
     }
 }
--- Source/Lokad.Cloud.Framework/Application/ServiceInspector.cs
+++ Source/Lokad.Cloud.Framework/Application/ServiceInspector.cs
@@ -5,129 +5,145 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Reflection;
 using Lokad.Cloud.ServiceFabric;
+using Mono.Cecil;
 
 namespace Lokad.Cloud.Application
 {
     /// <summary>
-    /// Utility to inspect cloud services in an isolated AppDomain.
+    /// Utility to inspect cloud services.
     /// </summary>
+    /// <remarks>
+    /// Mono.Cecil is used instead of .NET Reflector:
+    /// 1. So we don't need to use an AppDomain to be able to unload the assemblies afterwards.
+    /// 2. So we can reflect without resolving and loading any dependent assemblies (AutoFac, Lookad.Cloud.Storage)
+    /// </remarks>
     internal static class ServiceInspector
     {
         internal static ServiceInspectionResult Inspect(byte[] packageBytes)
         {
-            var sandbox = AppDomain.CreateDomain("ServiceInspector", null, AppDomain.CurrentDomain.SetupInformation);
-            try
-            {
-                var wrapper = (Wrapper)sandbox.CreateInstanceAndUnwrap(
-                    Assembly.GetExecutingAssembly().FullName,
-                    (typeof(Wrapper)).FullName,
-                    false,
-                    BindingFlags.CreateInstance,
-                    null,
-                    new object[] { packageBytes },
-                    null,
-                    new object[0]);
+            var reader = new CloudApplicationPackageReader();
+            var package = reader.ReadPackage(packageBytes, false);
 
-                return wrapper.Result;
-            }
-            finally
+            var cloudServiceTypeDefinitions = new List<TypeDefinition>();
+            var queueServiceTypeDefinitions = new List<TypeDefinition>();
+            var scheduledServiceTypeDefinitions = new List<TypeDefinition>();
+
+            var typeDefinitionMaps = new Dictionary<string, TypeDefinition>();
+            var serviceBaseTypes = new Dictionary<string, List<TypeDefinition>>
+                {
+                    { typeof(CloudService).FullName, cloudServiceTypeDefinitions },
+                    { typeof(ScheduledService).FullName, scheduledServiceTypeDefinitions },
+                    { typeof(QueueService<>).FullName, queueServiceTypeDefinitions }
+                };
+
+            // Instead of resolving, we reflect iteratively with multiple passes.
+            // This way we can avoid loading referenced assemblies like AutoFac
+            // and Lokad.Cloud.Storage which may have mismatching versions
+            // (e.g. Autofac 2 is completely incompatible to Autofac 1)
+
+            var assebliesBytes = package.Assemblies.Select(package.GetAssembly).ToList();
+            assebliesBytes.Add(File.ReadAllBytes(typeof(CloudService).Assembly.Location));
+
+            bool newTypesFoundThisRound;
+            do
             {
-                AppDomain.Unload(sandbox);
+                newTypesFoundThisRound = false;
+                foreach (var assemblyBytes in assebliesBytes)
+                {
+                    using (var stream = new MemoryStream(assemblyBytes))
+                    {
+                        var definition = AssemblyDefinition.ReadAssembly(stream);
+                        foreach (var typeDef in definition.MainModule.Types)
+                        {
+                            if (typeDef.BaseType == null || typeDef.BaseType.FullName == "System.Object" || serviceBaseTypes.ContainsKey(typeDef.FullName))
+                            {
+                                continue;
+                            }
+
+                            var baseTypeName = typeDef.BaseType.IsGenericInstance
+                                ? typeDef.BaseType.Namespace + "." + typeDef.BaseType.Name
+                                : typeDef.BaseType.FullName;
+
+                            List<TypeDefinition> matchingServiceTypes;
+                            if (!serviceBaseTypes.TryGetValue(baseTypeName, out matchingServiceTypes))
+                            {
+                                continue;
+                            }
+
+                            typeDefinitionMaps.Add(typeDef.FullName, typeDef);
+                            serviceBaseTypes.Add(typeDef.FullName, matchingServiceTypes);
+                            newTypesFoundThisRound = true;
+
+                            if (!typeDef.IsAbstract && !typeDef.HasGenericParameters)
+                            {
+                                matchingServiceTypes.Add(typeDef);
+                            }
+                        }
+                    }
+                }
             }
+            while (newTypesFoundThisRound);
+
+            return new ServiceInspectionResult
+                {
+                    CloudServices = cloudServiceTypeDefinitions.Select(td => new CloudServiceDefinition { TypeName = td.FullName }).ToList(),
+                    ScheduledServices = scheduledServiceTypeDefinitions.Select(td => new ScheduledServiceDefinition { TypeName = td.FullName }).ToList(),
+                    QueueServices = queueServiceTypeDefinitions.Select(td =>
+                        {
+                            var messageType = GetQueueServiceMessageType(td, typeDefinitionMaps);
+                            return new QueueServiceDefinition
+                                {
+                                    TypeName = td.FullName,
+                                    MessageTypeName = messageType.FullName,
+                                    QueueName = GetAttributeProperty(td, typeof(QueueServiceSettingsAttribute).FullName, "QueueName", () => messageType.FullName.ToLowerInvariant().Replace(".", "-"))
+                                };
+                        }).ToList()
+                };
         }
 
-        [Serializable]
+        static T GetAttributeProperty<T>(TypeDefinition type, string attributeName, string propertyName, Func<T> defaultValue)
+        {
+            var attribute = type.CustomAttributes.FirstOrDefault(ca => ca.AttributeType.FullName == attributeName);
+            if (attribute == null)
+            {
+                return defaultValue();
+            }
+
+            var property = attribute.Properties.FirstOrDefault(p => p.Name == propertyName);
+            if (property.Name == null)
+            {
+                return defaultValue();
+            }
+
+            return (T)property.Argument.Value;
+        }
+
+        static TypeReference GetQueueServiceMessageType(TypeDefinition typeDefinition, Dictionary<string, TypeDefinition> typeDefinitionMaps)
+        {
+            var baseRef = typeDefinition.BaseType;
+            var baseRefName = baseRef.Namespace + "." + baseRef.Name;
+            if (baseRefName == typeof(QueueService<>).FullName)
+            {
+                return ((GenericInstanceType)baseRef).GenericArguments[0];
+            }
+
+            var parentMessageType = GetQueueServiceMessageType(typeDefinitionMaps[baseRefName], typeDefinitionMaps);
+            if (!parentMessageType.IsGenericParameter)
+            {
+                return parentMessageType;
+            }
+
+            return ((GenericInstanceType)baseRef).GenericArguments[0];
+        }
+
         internal class ServiceInspectionResult
         {
-            public List<QueueServiceDefinition> QueueServices { get; set; }
-            public List<ScheduledServiceDefinition> ScheduledServices { get; set; }
             public List<CloudServiceDefinition> CloudServices { get; set; }
-        }
-
-        /// <summary>
-        /// Wraps an assembly (to be used from within a secondary AppDomain).
-        /// </summary>
-        private class Wrapper : MarshalByRefObject
-        {
-            internal ServiceInspectionResult Result { get; private set; }
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Wrapper"/> class.
-            /// </summary>
-            /// <param name="packageBytes">The application package bytes.</param>
-            public Wrapper(byte[] packageBytes)
-            {
-                var reader = new CloudApplicationPackageReader();
-                var package = reader.ReadPackage(packageBytes, false);
-                package.LoadAssemblies();
-
-                var serviceTypes = AppDomain.CurrentDomain.ReflectionOnlyGetAssemblies()
-                    .Select(a => a.GetExportedTypes()).SelectMany(x => x)
-                    .Where(t => t.IsSubclassOf(typeof(CloudService)) && !t.IsAbstract && !t.IsGenericType)
-                    .ToList();
-
-                var scheduledServiceTypes = serviceTypes.Where(t => t.IsSubclassOf(typeof(ScheduledService))).ToList();
-                var queueServiceTypes = serviceTypes.Where(t => IsSubclassOfRawGeneric(t, typeof(QueueService<>))).ToList();
-                var cloudServiceTypes = serviceTypes.Except(scheduledServiceTypes).Except(queueServiceTypes).ToList();
-
-                Result = new ServiceInspectionResult
-                    {
-                        ScheduledServices = scheduledServiceTypes
-                            .Select(t => new ScheduledServiceDefinition { TypeName = t.FullName }).ToList(),
-                        CloudServices = cloudServiceTypes
-                            .Select(t => new CloudServiceDefinition { TypeName = t.FullName }).ToList(),
-                        QueueServices = queueServiceTypes
-                            .Select(t =>
-                                {
-                                    var messageType = GetBaseClassGenericTypeParameters(t, typeof(QueueService<>))[0];
-
-                                    var attribute = t.GetCustomAttributes(
-                                        typeof(QueueServiceSettingsAttribute), true).FirstOrDefault() as QueueServiceSettingsAttribute;
-                                    var queueName = (attribute != null && !String.IsNullOrEmpty(attribute.QueueName))
-                                        ? attribute.QueueName
-                                        : TypeMapper.GetStorageName(messageType);
-
-                                    return new QueueServiceDefinition
-                                        {
-                                            TypeName = t.FullName,
-                                            MessageTypeName = messageType.FullName,
-                                            QueueName = queueName
-                                        };
-                                }).ToList()
-                    };
-            }
-
-            static bool IsSubclassOfRawGeneric(Type type, Type baseGenericTypeDefinition)
-            {
-                while (type != typeof(object))
-                {
-                    var cur = type.IsGenericType ? type.GetGenericTypeDefinition() : type;
-                    if (baseGenericTypeDefinition == cur)
-                    {
-                        return true;
-                    }
-                    type = type.BaseType;
-                }
-                return false;
-            }
-
-            static Type[] GetBaseClassGenericTypeParameters(Type type, Type baseGenericTypeDefinition)
-            {
-                while (type != typeof(object))
-                {
-                    var cur = type.IsGenericType ? type.GetGenericTypeDefinition() : type;
-                    if (baseGenericTypeDefinition == cur)
-                    {
-                        return type.GetGenericArguments();
-                    }
-                    type = type.BaseType;
-                }
-                
-                throw new InvalidOperationException();
-            }
+            public List<ScheduledServiceDefinition> ScheduledServices { get; set; }
+            public List<QueueServiceDefinition> QueueServices { get; set; }
         }
     }
 }
