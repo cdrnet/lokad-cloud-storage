f962aaa065a7bfb6ba646ad91c5550654582073a
Christoph Ruegg <christoph.ruegg@lokad.com>
2011-11-15 18:48:55 +0100

    Blobs: ITypedBlobName as typed equivalent to IBlobName

--- Source/Lokad.Cloud.Storage/Blobs/BlobName.cs
+++ Source/Lokad.Cloud.Storage/Blobs/BlobName.cs
@@ -14,7 +14,7 @@ namespace Lokad.Cloud.Storage
     /// </summary>
     /// <typeparam name="T">Type contained in the blob.</typeparam>
     [Serializable, DataContract]
-    public abstract class BlobName<T> : UntypedBlobName
+    public abstract class BlobName<T> : UntypedBlobName, ITypedBlobName<T>
     {
     }
 }
