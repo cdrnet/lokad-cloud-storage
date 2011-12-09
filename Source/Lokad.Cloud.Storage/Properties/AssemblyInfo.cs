d4299a3dc3bec3f022fa879974aeb9fe537b4a91
Christoph Ruegg <christoph.ruegg@lokad.com>
2011-12-09 15:47:27 +0100

    Build: re-normalize solution layout

--- Source/Lokad.Cloud.Storage/Properties/AssemblyInfo.cs
+++ Source/Lokad.Cloud.Storage/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright (c) Lokad 2009-2010
+﻿#region Copyright (c) Lokad 2009-2011
 // This code is released under the terms of the new BSD licence.
 // URL: http://www.lokad.com/
 #endregion
@@ -7,15 +7,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Lokad.Cloud.Storage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCopyright("Copyright © Lokad 2011")]
 [assembly: AssemblyTrademark("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyCompany("Lokad")]
+[assembly: AssemblyProduct("Lokad.Cloud.Storage")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
 [assembly: Guid("f1c9ee22-a41c-47ce-8d7e-bcacd3954986")]
 
 [assembly: InternalsVisibleTo("Lokad.Cloud.Storage.Test")]
